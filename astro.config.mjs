69b4b6fa023841263181c54fb62f1b856bc3b660
Keith Robinson <keith.robinson@dtn.com>
2024-02-23 10:48:56 -0700

    Refactor chunk, add packages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { ANALYTICS, SITE } from './src/utils/config.ts';
 import { defineConfig, squooshImageService } from 'astro/config';
+import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin } from './src/utils/frontmatter.mjs';
+
+import compress from 'astro-compress';
+import { fileURLToPath } from 'url';
+import icon from 'astro-icon';
+import mdx from '@astrojs/mdx';
+import netlify from '@astrojs/netlify';
+import partytown from '@astrojs/partytown';
+import path from 'path';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import partytown from '@astrojs/partytown';
-import icon from 'astro-icon';
-import compress from 'astro-compress';
 import tasks from './src/utils/tasks';
-import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin } from './src/utils/frontmatter.mjs';
-import { ANALYTICS, SITE } from './src/utils/config.ts';
-import netlify from '@astrojs/netlify';
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const whenExternalScripts = (items = []) =>
   ANALYTICS.vendors.googleAnalytics.id && ANALYTICS.vendors.googleAnalytics.partytown
