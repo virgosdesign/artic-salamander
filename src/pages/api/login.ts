4d15d13d229fb3d31042bc38f868d52850fbedee
Keith Robinson <keith.robinson@dtn.com>
2024-03-10 09:28:53 -0700

    Authentication work.

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,12 +1,14 @@
-import { db, lucia, userTable } from "~/auth";
+import { db, lucia } from "~/auth"
 
-import type { APIContext } from "astro";
-import { Argon2id } from "oslo/password";
+import type { APIContext } from "astro"
+import { Argon2id } from "oslo/password"
 import { eq } from 'drizzle-orm'
+import { userTable } from "~/data/schema"
 
 export async function POST(context: APIContext): Promise<Response> {
-	const formData = await context.request.formData();
-	const username = formData.get("username");
+	const formData = await context.request.formData()
+	
+  const username = formData.get("username")
 	if (
 		typeof username !== "string" ||
 		username.length < 3 ||
@@ -15,19 +17,35 @@ export async function POST(context: APIContext): Promise<Response> {
 	) {
 		return new Response("Invalid username", {
 			status: 400
-		});
+		})
 	}
-	const password = formData.get("password");
-	if (typeof password !== "string" || password.length < 6 || password.length > 255) {
+
+	const password = formData.get("password")
+	if (
+    typeof password !== "string" ||
+    password.length < 6 ||
+    password.length > 255
+  ) {
 		return new Response("Invalid password", {
 			status: 400
-		});
+		})
 	}
+
+  const email = formData.get('email')
+  if (
+    typeof email !== "string" || 
+    email.length < 6 ||
+    email.length > 255
+  ) {
+    return new Response("Invalid email", {
+      status: 400
+    })
+  }
 
 const existingUser = await db
   .select()
   .from(userTable)
-  .where(eq(userTable.username, username))
+  .where(eq(userTable.email, email))
 	if (!existingUser) {
 		// NOTE:
 		// Returning immediately allows malicious actors to figure out valid usernames from response times,
@@ -40,7 +58,7 @@ const existingUser = await db
 		// If usernames are public, you may outright tell the user that the username is invalid.
 		return new Response("Incorrect username or password", {
 			status: 400
-		});
+		})
 	}
 
 	if (existingUser[0].hashed_password) {
@@ -48,15 +66,15 @@ const existingUser = await db
     if (!validPassword) {
       return new Response("Incorrect username or password", {
         status: 400
-      });
+      })
     }
   }
 
-	const session = await lucia.createSession(existingUser[0].id, {});
-	const sessionCookie = lucia.createSessionCookie(session.id);
-	context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+	const session = await lucia.createSession(existingUser[0].id, {})
+	const sessionCookie = lucia.createSessionCookie(session.id)
+	context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes)
 
-  console.log("******************* cookies *******************", session);
-  console.log("******************* session *******************", context.cookies.get(sessionCookie.name));
-	return context.redirect("/dashboard");
+  console.log("******************* cookies *******************", session)
+  console.log("******************* session *******************", context.cookies.get(sessionCookie.name))
+	return context.redirect("/dashboard")
 }
